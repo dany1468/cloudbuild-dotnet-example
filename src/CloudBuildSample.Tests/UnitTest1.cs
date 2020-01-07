c545500c6823cbebc3c414d8740842f8248d319a
dany1468 <dany1468@gmail.com>
2020-01-07 18:51:59 +0900

    Add a test that insert data and assert row count

--- src/CloudBuildSample.Tests/UnitTest1.cs
+++ src/CloudBuildSample.Tests/UnitTest1.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Linq;
+using CloudBuildSample.EF;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 
 namespace CloudBuildSample.Tests
@@ -8,7 +11,24 @@ namespace CloudBuildSample.Tests
         [Fact]
         public void Test1()
         {
+            var connectionString = Environment.GetEnvironmentVariable("CONN_STRING");
 
+            var options = new DbContextOptionsBuilder<ProductsDbContext>().UseSqlServer(connectionString);
+            using var context = new ProductsDbContext(options.Options);
+
+            context.Database.ExecuteSqlRaw("DELETE FROM dbo.Products;");
+
+            context.Products.Add(new Product()
+            {
+                ProductName = "Mac Book Pro",
+                UnitPrice = 1000000
+            });
+
+            context.SaveChanges();
+
+            var count = context.Products.Count();
+
+            Assert.Equal(1, count);
         }
     }
 }
