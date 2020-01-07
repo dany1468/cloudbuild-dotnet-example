bcd19dcfc9c732a7d393dc3880b3ec57d2dac722
dany1468 <dany1468@gmail.com>
2020-01-07 17:21:35 +0900

    Add an Entity and a DbContext, and a DbContextFactory

--- src/CloudBuildSample.EF/Class1.cs
+++ src/CloudBuildSample.EF/Class1.cs
@@ -1,8 +1,31 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
 
 namespace CloudBuildSample.EF
 {
-    public class Class1
+    public class Product
     {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+
+    public class ProductsDbContext : DbContext
+    {
+        public ProductsDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+    public class ProductsDbContextFactory : IDesignTimeDbContextFactory<ProductsDbContext>
+    {
+        public ProductsDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ProductsDbContext>();
+            var connectionString = Environment.GetEnvironmentVariable("CONN_STRING");
+            optionsBuilder.UseSqlServer(connectionString, b => b.MigrationsAssembly("CloudBuildSample.EF"));
+            return new ProductsDbContext(optionsBuilder.Options);
+        }
     }
 }
